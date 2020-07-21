f0c36e7d0b776bf2c585707371664561f8a2f89a
DoubleHelix <tjrnyc5@gmail.com>
2020-07-21 13:55:34 -0400

    code cleaning

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Arjun is a biechhhhhhhh")
+    return jsonify("Healthy")
 
 
 @APP.route('/auth', methods=['POST'])
